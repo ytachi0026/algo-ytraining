c41af9c48e23eab9fcda25b842f90017cba345e9
ytachi <ytaloborjam@gmail.com>
2020-05-22 10:37:05 -0500

    devytachi: adding solution to sherlock valid strings

--- src/main/java/dev/ytachi/hackerrank/interview/kit/string/manipulation/SherlockAndTheValidString.java
+++ src/main/java/dev/ytachi/hackerrank/interview/kit/string/manipulation/SherlockAndTheValidString.java
@@ -20,31 +20,31 @@ public class SherlockAndTheValidString {
     static String isValid(String s) {
         long[] ocurrences = new long[26]; // since the problem states S[i] belongs to asccis [a-z]
 
-        // lets calcula te the frequency of each S character
+        // lets calculate the frequency of each S character
         for (int i = 0; i < s.length(); i++) {
             ocurrences[s.charAt(i) - 'a']++;
         }
         // think about it, you can delete, but not add, if we order this, we will be good
         // Or you can just don't order it, since you need to delete 1!
 
-        long currentFrequency = -1;
+        long maxFrequency = -1;
         boolean removedChar = false;
         // lets analyse it
         for (int i = 0; i < 26; i++) {
             if (ocurrences[i] == 0) {
                 continue;
             } else {
-                if (currentFrequency == -1) {
-                    currentFrequency = ocurrences[i]; // lets assume this is gonna be the default frequency, since it is the lower one.
+                if (maxFrequency == -1) {
+                    maxFrequency = ocurrences[i]; // lets assume this is gonna be the default frequency, since it is the lower one.
                     continue;
-                } else if (currentFrequency == ocurrences[i]) {
+                } else if (maxFrequency == ocurrences[i]) {
                         continue;
-                    } else if (!removedChar && Math.abs(ocurrences[i] - currentFrequency) == 1) {
+                    } else if (!removedChar && ((ocurrences[i] == maxFrequency + 1) || ocurrences[i] == 1)) {
                         removedChar = true;
                         continue;
                     } else {
-                    return "NO";
-                }
+                        return "NO";
+                    }
             }
         }
         return "YES";
