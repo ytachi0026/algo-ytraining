94f32df2b241151d3fa6a7fd05e8b0138df2ac95
Ytalo Borja <ytaloborja@gmail.com>
2020-10-04 09:11:16 -0500

    devytachi: adding luck balance, be aware of the problem

---
+++ src/main/java/dev/ytachi/hackerrank/interview/kit/greedy/algorithms/LuckBalance.java
@@ -0,0 +1,45 @@
+package dev.ytachi.hackerrank.interview.kit.greedy.algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LuckBalance {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    // Complete the luckBalance function below.
+    // k: the number of important contests Lena can lose
+    static int luckBalance(int k, int[][] contests) {
+        int maxLuck = 0;
+        // I think i need to sort the contest
+        List important = new ArrayList<Integer>();
+
+        for (int i = 0; i < contests.length; i++) {
+            // You are loosing all the unimportant ones!
+            if (contests[i][1] == 0) {
+                maxLuck += contests[i][0];
+            } else {
+                important.add(contests[i][0]);
+            }
+        }
+
+        //Sort the important contests in descending order
+        Collections.sort(important, Collections.reverseOrder());
+
+        //Lose the k largest contests and win the rest
+        // because if you lose the important contest, you win LUCK!!, and to maximise, you need luck!
+        for (int i = 0; i < important.size(); i++) {
+            if (i < k) {
+                maxLuck += (int) important.get(i);
+            } else {
+                maxLuck -= (int) important.get(i);
+            }
+        }
+
+        return maxLuck;
+    }
+}
